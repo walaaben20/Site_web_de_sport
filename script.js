f2fbdbd22c4cf54c4c8ca0c3b622ff2ee15dbea3
walaaben20 <walabhy2022@gmail.com>
2024-03-03 20:12:33 +0100

    modif de la bare de connexion

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ boutonAlerte.addEventListener("click", function() {
     // Afficher une boîte de dialogue avec un message d'alerte
     alert("Bienvenue sur notre site de sport !");
 });
+// Sélectionner le formulaire
+var form = document.querySelector('form');
+
+// Ajouter un écouteur d'événement pour le submit du formulaire
+form.addEventListener('submit', function(event) {
+    // Empêcher le comportement par défaut du formulaire
+    event.preventDefault();
+
+    // Récupérer les valeurs des champs
+    var username = document.getElementById('username').value;
+    var birthdate = document.getElementById('birthdate').value;
+    var location = document.getElementById('location').value;
+
+    // Afficher un message de bienvenue
+    alert('Bienvenue, ' + username + '! Vous êtes né le ' + birthdate + ' à ' + location + '.');
+});
