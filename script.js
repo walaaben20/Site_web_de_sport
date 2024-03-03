0e874d78945f1f120317b7f03ae01ec7cf1a6caa
walaaben20 <walabhy2022@gmail.com>
2024-03-03 16:21:54 +0100

    ajout_de_code_js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sélectionner le bouton
+var boutonAlerte = document.getElementById("bouton-alerte");
+
+// Ajouter un gestionnaire d'événement au clic sur le bouton
+boutonAlerte.addEventListener("click", function() {
+    // Afficher une boîte de dialogue avec un message d'alerte
+    alert("Bienvenue sur notre site de sport !");
+});
